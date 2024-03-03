58e654e28a01c629acae7b91089611c8ffcf1320
rohit.chopade <rohit.chopade@YP1016088LT>
2024-03-03 14:00:26 +0530

    Add project files.

---
+++ DIce/PhoneBook.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIce
+{
+    class PhoneBook
+    {
+
+        private List<Contact>_contacts = new List<Contact>();
+       
+
+        private void Dispalyrecord(Contact contact)
+        {
+           
+            Console.WriteLine($"contactdeails:{contact.Name}{ contact.Number}");
+
+        }
+        private void Dispalyrecord(List<Contact> _contacts)
+        {
+            foreach (var itam in _contacts)
+            {
+                Console.WriteLine(itam);
+            }
+
+
+        }
+
+        public void Createacontact(Contact contact)
+        {
+            _contacts.Add(contact);
+              
+        }
+        public void DisplayContact(string number)
+        {
+            var contact=_contacts.FirstOrDefault(C=>C.Number==number);
+            if(contact==null)
+            {
+                Console.WriteLine("invalid number");
+            }
+            else
+            {
+                Dispalyrecord(contact);
+            }
+
+        }
+        public void DisplayallContact()
+        {
+           foreach(var contact in _contacts)
+            {
+                Dispalyrecord(contact);
+            }
+        }
+
+        public void matchingphrase(string serachph)
+        {
+
+            var matchingContact = _contacts.Where(c => c.Name.Contains(serachph)).ToList();
+
+            Dispalyrecord(matchingContact);
+        }
+
+    }
+
+
+}
---
+++ DIce/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DIce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Well Come phonebook");
+
+            Console.WriteLine("1 for create contact");
+
+            Console.WriteLine("2 for Get all contact");
+
+            Console.WriteLine("3 for Get Name by passing number");
+
+            Console.WriteLine("search Contact");
+
+            var inpute=Console.ReadLine();
+            var phonebooks = new PhoneBook();
+            while(true)
+            {
+                switch (inpute)
+                {
+                    case "1":
+
+                        Console.WriteLine("input name");
+                        var name = Console.ReadLine();
+                        Console.WriteLine("input number");
+                        var number = Console.ReadLine();
+
+                        var newcontact = new Contact(name, number);
+
+                        phonebooks.Createacontact(newcontact);
+                        break;
+
+
+
+
+                        break;
+                    case "2":
+                        phonebooks.DisplayallContact();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("input number");
+                        var getname = Console.ReadLine();
+
+                        phonebooks.matchingphrase(getname);
+                        break;
+
+                    case "x":
+                        return;
+                    default:
+                        Console.WriteLine("invalid option");
+                        break;
+                }
+                Console.WriteLine("select operation");
+                inpute = Console.ReadLine();
+
+            }
+
+            
+
+
+
+        }
+        
+    }
+}
